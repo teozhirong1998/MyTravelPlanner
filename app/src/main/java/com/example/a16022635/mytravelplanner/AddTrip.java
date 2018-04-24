232fa47f075be4522bfbd2d09336e4b9399fd428
teozhirong1998 <16022635@myrp.edu.sg>
2018-04-24 22:21:44 +0800

    Update commit

--- app/src/main/java/com/example/a16022635/mytravelplanner/AddTrip.java
+++ app/src/main/java/com/example/a16022635/mytravelplanner/AddTrip.java
@@ -5,9 +5,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class AddTrip extends AppCompatActivity {
 
@@ -16,6 +19,9 @@ public class AddTrip extends AppCompatActivity {
     ImageView imageToUpload;
     Button btnUpload;
     Button btnSaveTrip;
+    EditText etTripTitle, etTripDuration, etTripLocation;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,9 @@ public class AddTrip extends AppCompatActivity {
         imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
         btnUpload = (Button) findViewById(R.id.buttonUpload);
         btnSaveTrip = (Button) findViewById(R.id.btnSaveTrip);
+        etTripTitle = (EditText) findViewById(R.id.etTripTitle);
+        etTripDuration = (EditText) findViewById(R.id.etTripDuration);
+        etTripLocation = (EditText) findViewById(R.id.etTripLocation) ;
 
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,9 +45,30 @@ public class AddTrip extends AppCompatActivity {
             }
         });
 
-        
 
-    }
+            btnSaveTrip.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if((etTripTitle.getText().toString().equals("")) && (etTripDuration.getText().toString().equals("")) && (etTripLocation.getText().toString().equals(""))) {
+                        Toast.makeText(getApplicationContext(),"Please fill up the blanks, image can be left empty.", Toast.LENGTH_LONG).show();
+                    }else {
+                        String tripTitle = etTripTitle.getText().toString();
+                        String tripDuration = etTripDuration.getText().toString();
+                        String tripLocation = etTripLocation.getText().toString();
+                        Intent i = new Intent(AddTrip.this, AddTripDetails.class);
+
+                        i.putExtra("tripTitle", tripTitle);
+                        i.putExtra("tripDuration", tripDuration);
+                        i.putExtra("tripLocation", tripLocation);
+                        startActivity(i);
+                    }
+
+
+                }
+            });
+        }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
